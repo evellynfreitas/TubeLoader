b12922ada293d270bdadd50a35438b609a578c06
evellynfreitas <evellyndfreitas@gmail.com>
2024-04-23 12:42:07 -0300

    Função: baixar playlist

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from pytube import YouTube, Playlist
 def baixarVideo(url):
     video = YouTube(url)
     video.streams.get_highest_resolution().download()
+
+
+def baixarPlaylist(url_playlist):
+    playlist = Playlist(url_playlist)
+
+    for url in playlist:
+        baixarVideo(url)
