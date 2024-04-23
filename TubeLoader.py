453ed9f1beaf8f615468c21d668afa30ac72b6f2
evellynfreitas <evellyndfreitas@gmail.com>
2024-04-23 16:23:47 -0300

    Feature: salvar em um diretório

--- TubeLoader.py
+++ TubeLoader.py
@@ -1,13 +1,13 @@
 from pytube import YouTube, Playlist
 
 
-def baixarVideo(url):
+def baixarVideo(url, pasta):
     video = YouTube(url)
-    video.streams.get_highest_resolution().download()
+    video.streams.get_highest_resolution().download(pasta)
 
 
-def baixarPlaylist(url_playlist):
+def baixarPlaylist(url_playlist, pasta):
     playlist = Playlist(url_playlist)
 
     for url in playlist:
-        baixarVideo(url)
+        baixarVideo(url, pasta)
